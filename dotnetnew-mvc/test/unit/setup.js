c10546552df035b247ef76523c3d343afa709137
diegohb <diegohb@bustamantenow.com>
2017-08-09 19:39:10 -0400

    au new --here using SystemJS (didnt install packages)

---
+++ dotnetnew-mvc/test/unit/setup.js
@@ -0,0 +1,6 @@
+define(["require", "exports", "aurelia-pal-browser", "aurelia-polyfills"], function (require, exports, aurelia_pal_browser_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    aurelia_pal_browser_1.initialize();
+});
+//# sourceMappingURL=setup.js.map
