bcb424df7ed35bf5be024badf97c9176a0b37f76
diegohb <diegohb@bustamantenow.com>
2017-08-10 16:57:41 -0400

    add gulp tasks and binding

--- dotnetnew-mvc/gulpfile.js
+++ dotnetnew-mvc/gulpfile.js
@@ -1,5 +1,9 @@
-﻿/// <binding AfterBuild='bulid' />
+﻿/// <binding ProjectOpened='au-build-watch' />
 var gulp = require("gulp");
 var shell = require("gulp-shell");
  
-gulp.task("bulid", shell.task(["au build"]));
+gulp.task("au-build", shell.task(["au build"]));
+gulp.task("au-build-watch", shell.task(["au build --watch"]));
+gulp.task("au-browsersync", shell.task(["au run"]));
+gulp.task("au-browsersync-watch", shell.task(["au run --watch"]));
+gulp.task("au-build-prod", shell.task(["au build --env prod"]));
