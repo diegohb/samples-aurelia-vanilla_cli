7a7475360ce3184a53357af9a2cbe21b692f0d52
diegohb <diegohb@bustamantenow.com>
2017-08-10 19:53:59 -0400

    configured aurelia main to take param for root module name. (http://patrickwalters.net/creating-multipage-apps-using-aurelia-2/)

--- dotnetnew-mvc/src/main.ts
+++ dotnetnew-mvc/src/main.ts
@@ -1,18 +1,21 @@
-import {Aurelia} from 'aurelia-framework'
-import environment from './environment';
+import {Aurelia} from "aurelia-framework"
+import environment from "./environment";
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature('resources');
+    .feature("resources");
 
   if (environment.debug) {
     aurelia.use.developmentLogging();
   }
 
   if (environment.testing) {
-    aurelia.use.plugin('aurelia-testing');
+    aurelia.use.plugin("aurelia-testing");
   }
 
-  aurelia.start().then(() => aurelia.setRoot());
+    aurelia.start().then((pAurelia:Aurelia) => {
+        let startModuleName = (<any>pAurelia.host.attributes).start.value;
+        pAurelia.setRoot(startModuleName);
+    });
 }
