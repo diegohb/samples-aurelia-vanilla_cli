fa47781eda5766daa0aeee30fa5d238bfa9f608c
diegohb <diegohb@bustamantenow.com>
2017-08-15 13:48:28 -0400

    Made changes to define optional sections in aspnet mvc at Layout with default content. This enables having regular aspnet razor views that can leverage bootstrap and jquery from CDN. this works side-by-side with the SPA deployments which bundle CSS + JS into single files included using SystemJS. -deleted lib directory. -manually built out site.min.css file

--- dotnetnew-mvc/src/main.ts
+++ dotnetnew-mvc/src/main.ts
@@ -1,4 +1,4 @@
-import {Aurelia} from "aurelia-framework"
+import { Aurelia } from "aurelia-framework"
 import environment from "./environment";
 
 export function configure(aurelia: Aurelia) {
@@ -7,15 +7,17 @@ export function configure(aurelia: Aurelia) {
         .plugin("aurelia-bootstrap")
         .feature("resources");
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+    aurelia.use.globalResources("bootstrap/css/bootstrap.css");
 
-  if (environment.testing) {
-    aurelia.use.plugin("aurelia-testing");
-  }
+    if (environment.debug) {
+        aurelia.use.developmentLogging();
+    }
 
-    aurelia.start().then((pAurelia:Aurelia) => {
+    if (environment.testing) {
+        aurelia.use.plugin("aurelia-testing");
+    }
+
+    aurelia.start().then((pAurelia: Aurelia) => {
         let startModuleName = (<any>pAurelia.host.attributes).start.value;
         pAurelia.setRoot(startModuleName);
     });
