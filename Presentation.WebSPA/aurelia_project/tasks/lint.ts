3b01980c98adb72aa25f317c639d9d96cc05c0b5
diegohb <diegohb@bustamantenow.com>
2019-07-05 16:09:27 -0400

    tried.. aborted..

--- Presentation.WebSPA/aurelia_project/tasks/lint.ts
+++ Presentation.WebSPA/aurelia_project/tasks/lint.ts
@@ -1,12 +1,14 @@
 import * as gulp from 'gulp';
-import tslint from 'gulp-tslint';
+import tslint from 'eslint';
 import * as project from '../aurelia.json';
 
 export default function lint() {
   return gulp.src([project.transpiler.source])
-    .pipe(tslint({
+    .pipe(tslint.Linter({
       tslint: require("tslint"),
       formatter: 'prose'
     }))
     .pipe(tslint.report());
 }
+
+//https://stackoverflow.com/questions/36136737/gulp-eslint-doesnt-find-my-eslintrc-file
