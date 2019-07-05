0a7f9f1e124d0df6bc139955905bf3f21782180a
diegohb <diegohb@bustamantenow.com>
2019-07-05 13:55:12 -0400

    added comments fr optional features

--- Presentation.WebSPA/src/main.ts
+++ Presentation.WebSPA/src/main.ts
@@ -16,7 +16,16 @@ export function configure(aurelia: Aurelia) {
         aurelia.use.plugin("aurelia-testing");
     }
 
+    //Uncomment the line below to enable animation.
+    // aurelia.use.plugin('aurelia-animator-css');
+    //if the css animator is enabled, add swap-order="after" to all router-view elements
+
+    //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+    // aurelia.use.plugin('aurelia-html-import-template-loader');
+
     aurelia.start().then((pAurelia: Aurelia) => {
+        //Following enables multi-spa support with each container 
+        //specifying via html-attribute what spa module to load
         let startModuleName = (<any>pAurelia.host.attributes).start.value;
         pAurelia.setRoot(startModuleName);
         $("#loader").fadeOut("slow");
