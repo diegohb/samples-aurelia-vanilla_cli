ee133516e40c23b17ed33a145d76049eca1569df
diegohb <diegohb@bustamantenow.com>
2020-02-26 22:39:24 -0500

    -update packages -add console logging package -update to latest impl for logging -ran cleanups

--- Presentation.WebSPA/Controllers/HomeController.cs
+++ Presentation.WebSPA/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Presentation.WebSPA.Controllers
+﻿namespace Presentation.WebSPA.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     public class HomeController : Controller
     {
         public IActionResult Index()
--- Presentation.WebSPA/Startup.cs
+++ Presentation.WebSPA/Startup.cs
@@ -1,66 +1,71 @@
-﻿using System.IO;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Logging;
-
-namespace Presentation.WebSPA
+﻿namespace Presentation.WebSPA
 {
-  public class Startup
-  {
-    public Startup(IHostingEnvironment env)
+    using System.IO;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.FileProviders;
+    using Microsoft.Extensions.Logging;
+
+    public class Startup
     {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-          .AddEnvironmentVariables();
-      Configuration = builder.Build();
-    }
-
-    public IConfigurationRoot Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      // Add framework services.
-      services.AddMvc();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-    {
-      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-      loggerFactory.AddDebug();
-
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseBrowserLink();
-
-        //This allows you to debug your ts files in browser using the mappings provided by gulp-typescript
-        app.UseStaticFiles(new StaticFileOptions()
+        public Startup(IHostingEnvironment env)
         {
-          FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"src")),
-          RequestPath = new PathString("/src")
-        });
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-      }
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
 
-      app.UseStaticFiles();
+        public IConfigurationRoot Configuration { get; }
 
-      app.UseMvc(routes =>
-      {
-        routes.MapRoute(
-                  name: "default",
-                  template: "{controller=Home}/{action=Index}/{id?}");
-      });
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            services.AddLogging(pLoggingBuilder =>
+            {
+                pLoggingBuilder.AddConfiguration(Configuration.GetSection("Logging"));
+                pLoggingBuilder.AddConsole(pOpts => pOpts.IncludeScopes = true);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+
+                //This allows you to debug your ts files in browser using the mappings provided by gulp-typescript
+                app.UseStaticFiles
+                (new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"src")),
+                    RequestPath = new PathString("/src")
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc
+            (routes =>
+            {
+                routes.MapRoute
+                (
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
     }
-  }
 }
