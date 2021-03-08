5bc1913c201188195456b90522502a221e358929
Diego H. Bustamante <diegohb@expertvision.software>
2021-03-08 03:57:57 +0000

    change model to class

--- Presentation.WebSPA/src/routes/rolodex-spa/models/contact-model.ts
+++ Presentation.WebSPA/src/routes/rolodex-spa/models/contact-model.ts
@@ -1,7 +1,7 @@
-﻿export interface Contact {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    phoneNumber?: string;
+﻿export class Contact {
+    public id: number;
+    public firstName: string;
+    public lastName: string;
+    public email: string;
+    public phoneNumber?: string;
 }
