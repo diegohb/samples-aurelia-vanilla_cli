b94e423100c5b02fc5c07b4e8a58f7dd4f852c42
Diego H. Bustamante <diegohb@expertvision.software>
2021-03-09 04:04:20 +0000

    implement dummy api (fake data)

--- Presentation.WebSPA/src/routes/rolodex-spa/services/api.ts
+++ Presentation.WebSPA/src/routes/rolodex-spa/services/api.ts
@@ -1,5 +1,8 @@
-﻿import { delay } from "../services/utility";
+﻿import { autoinject } from "aurelia-framework";
+import { delay } from "../services/utility";
 import { Contact } from "../models/contact-model";
+import { HttpClient } from "aurelia-fetch-client";
+import { ApiLoggerInterceptor } from "./ApiLoggerInterceptor";
 
 let latency = 600;
 let id = 0;
@@ -8,7 +11,7 @@ function getId() {
     return ++id;
 }
 
-let contacts: Array<Contact> = [
+/*let contacts: Array<Contact> = [
     {
         id: getId(),
         firstName: "John",
@@ -44,15 +47,72 @@ let contacts: Array<Contact> = [
         email: "green@inklings.com",
         phoneNumber: "867-5309"
     }
-];
+];*/
 
+type ResponseDTO<TDataType> = {
+    data: Array<TDataType>;
+    limit: number;
+    offset: number;
+    page: number;
+    total: number;
+};
+
+type PersonDTO =
+{
+    id: string;
+    title: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    picture: string; //uri
+    gender?: "male" | "female" | "other" | "";
+    dateOfBirth?: string;
+    phone?: string;
+    location?: LocationDTO;
+};
+
+type LocationDTO = {
+    street: string;
+    city: string;
+    state: string;
+    country: string;
+    timezone: string;
+};
+
+
+
+@autoinject()
 export class Api {
+    private contacts: Array<Contact>;
+
+    constructor(private _http: HttpClient) {
+        _http.configure(config => {
+            config
+                .useStandardConfiguration()
+                .withBaseUrl("https://dummyapi.io/data/api/")
+                .withDefaults({
+                    credentials: "same-origin",
+                    headers: {
+                        "app-id": "6046baa083c2165d2b23ff80"
+                    }
+                })
+                .withInterceptor(new ApiLoggerInterceptor());
+        });
+
+        this._loadData();
+    }
+
     public isRequesting = false;
 
     public async getContactList(): Promise<Array<Contact>> {
         this.isRequesting = true;
+
+        if (!this.contacts || !this.contacts.length) {
+            await this._loadData();
+        }
+
         await delay(latency);
-        const results = contacts.map(x => {
+        const results = this.contacts.map(x => {
             return {
                 id: x.id,
                 firstName: x.firstName,
@@ -67,7 +127,7 @@ export class Api {
     public async getContactDetails(id:string): Promise<Contact> {
         this.isRequesting = true;
         await delay(latency);
-        const found = contacts.filter(x => x.id === Number.parseInt(id))[0];
+        const found = this.contacts.filter(x => x.id === Number.parseInt(id))[0];
         this.isRequesting = false;
         return found;
     }
@@ -76,17 +136,31 @@ export class Api {
         this.isRequesting = true;
         await delay(latency);
         const instance: Contact = contact;
-        const found: Contact = contacts.filter(x => x.id === contact.id)[0];
+        const found: Contact = this.contacts.filter(x => x.id === contact.id)[0];
 
         if (found) {
-            const index = contacts.indexOf(found);
-            contacts[index] = instance;
+            const index = this.contacts.indexOf(found);
+            this.contacts[index] = instance;
         } else {
             instance.id = getId();
-            contacts.push(instance);
+            this.contacts.push(instance);
         }
 
         this.isRequesting = false;
         return instance;
+    }
+
+    private async _loadData() {
+        const rawResponse = await this._http.fetch("user?limit=5");
+        const response: ResponseDTO<PersonDTO> = await rawResponse.json();
+        this.contacts = response.data.map((dto: PersonDTO) => {
+            const contact = <Contact>{
+                id: getId(),
+                firstName: dto.firstName,
+                lastName: dto.lastName,
+                email: dto.email
+            };
+            return contact;
+        });
     }
 }
