90c0cd3c6308d0e0c297077346f36c91f558f244
Diego H. Bustamante <diegohb@expertvision.software>
2021-03-13 07:09:50 +0000

    add custom element and dateformat from moment install earlier

--- Presentation.WebSPA/src/resources/index.ts
+++ Presentation.WebSPA/src/resources/index.ts
@@ -1,5 +1,8 @@
 import {FrameworkConfiguration} from "aurelia-framework";
 
 export function configure(config: FrameworkConfiguration) {
-  config.globalResources(["./value-converters/date-format-value-converter"]);
+    config.globalResources([
+        "./value-converters/date-format-value-converter",
+        "./elements/lr-single-selector"
+    ]);
 }
