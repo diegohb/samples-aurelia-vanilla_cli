fc52fb615691995b90492a6b47886e5e65ec5af4
Diego H. Bustamante <diegohb@expertvision.software>
2021-03-14 03:53:07 +0000

    styling to masonry cards
    included hacky fixes for 2 files that didnt match convention

--- Presentation.WebSPA/src/routes/pokemon-spa/pokemon-spa.ts
+++ Presentation.WebSPA/src/routes/pokemon-spa/pokemon-spa.ts
@@ -19,7 +19,15 @@ export class PokemonSPAViewModel {
     }
 
     public getImageUrlForPokemon(id: number): string {
+        if (!id)
+            return "";
+
         let imageFileName = id.toString().padStart(3, "0");
+        if (id === 662)
+            imageFileName = "662r";
+        else if (id === 740)
+            imageFileName = "740le";
+
         const url = `https://github.com/fanzeyi/pokemon.json/raw/master/images/${imageFileName}.png`;
         return url;
     }
