c1b4aafebb294975e1ab745924d69cf1a69c13bc
Diego H. Bustamante <diegohb@expertvision.software>
2021-03-14 02:27:27 +0000

    cached to localStorage

--- Presentation.WebSPA/src/routes/pokemon-spa/services/pokemon-svc.ts
+++ Presentation.WebSPA/src/routes/pokemon-spa/services/pokemon-svc.ts
@@ -1,6 +1,7 @@
 ﻿import { autoinject, LogManager } from "aurelia-framework";
 import { HttpClient } from "aurelia-fetch-client";
 import { PokemonSummaryModel } from "../models/pokemon-summary-model";
+import { PokemonDTO } from "../models/api-dto";
 
 @autoinject()
 export class PokemonService {
@@ -15,10 +16,24 @@ export class PokemonService {
     }
 
     public async fetchPokemonSummaries(): Promise<PokemonSummaryModel[]> {
-        const rawResponse = await this._http.fetch("pokemon");
-        const dtos = await rawResponse.json();
-        let models = dtos.map(dto => PokemonSummaryModel.fromDTO(dto));
+        const dtos = await this.getFromCacheOrFetch();
+        const models = dtos.map(dto => PokemonSummaryModel.fromDTO(dto));
         return models;
+    }
+
+    private async getFromCacheOrFetch(): Promise<PokemonDTO[]> {
+        let data: PokemonDTO[];
+        let raw: string = window.localStorage.getItem("pokedex.pokemons");
+        if (!raw) {
+            const rawResponse = await this._http.fetch("pokemon");
+            raw = await rawResponse.text();
+            window.localStorage.setItem("pokedex.pokemons", raw);
+            this._logger.info("Cached to local storage.");
+        } else {
+            this._logger.info("Fetched from cache in local storage.");
+        }
+        data = JSON.parse(raw);
+        return data;
     }
 
 }
